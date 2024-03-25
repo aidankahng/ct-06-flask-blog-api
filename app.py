e99849746509186f831a0e585b169c8807be45cf
aidankahng <aidankahng@gmail.com>
2024-03-25 10:10:51 -0700

    added second route with JSON response

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ def hello_world():
     first = "John"
     return f"Hello there {first}!"
 
+@app.route("/test")
+def test():
+    my_dicts = []
+    for i in range(5):
+        a_dict = {
+            'id' : i+1,
+            'square' : i**2
+        }
+        my_dicts.append(a_dict)
+
+    return my_dicts
+
+
 ## In terminal to run
 ## flask --app hello run -p 5050
 ## if name is app.py:
