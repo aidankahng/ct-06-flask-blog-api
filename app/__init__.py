c25b9a2f1a6b6eca70565b36e4ddf926be179439
aidankahng <aidankahng@gmail.com>
2024-03-26 12:21:49 -0700

    added imports for flask-SQLAlchemy and psycopg2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
+# create instance of Flask
 app = Flask(__name__, instance_relative_config=True)
+
+#set config for app and sql database
+app.config.from_object(Config)
+
+# create an instance of SQLAlchemy called db
+db = SQLAlchemy(app)
 
 # need to import the routes to the application
 from . import routes
