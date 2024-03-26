5e0715a19b01b80e50a2f242cca8cca983875d58
aidankahng <aidankahng@gmail.com>
2024-03-26 14:19:09 -0700

    Created user table and obfuscated passwords using werkzeug

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@
 
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     # use db. to use SQLAlchemy types
@@ -10,8 +11,29 @@ class User(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__set_password(kwargs.get('password', ''))
+        
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
 
+
+    # now automatically will add and commit to database when creating the user
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+     # hashes the password for security
+    def __set_password(self, plaintext_password):
+        self.password = generate_password_hash(plaintext_password)
+        self.save()
+
+    def check_password(self, plaintext_password):
+        return check_password_hash(self.password, plaintext_password)
+
+# u = User(first_name="Bob", last_name="Dylan", email="bd@rad.com", username="thebobdylan", password="123")
